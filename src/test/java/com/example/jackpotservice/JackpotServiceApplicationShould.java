dd68fbe5ff99162a19e3e0a6a94061090848e8e6
dmitar-pep <danijel.mitar@porsche-ep.com>
2026-05-23 09:07:05 +0200

    chore: init project

---
+++ src/test/java/com/example/jackpotservice/JackpotServiceApplicationShould.java
@@ -0,0 +1,13 @@
+package com.example.jackpotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackpotServiceApplicationShould {
+
+    @Test
+    void loadContext() {
+    }
+
+}
